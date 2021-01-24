dd7e37aa5b0ed17f2de87e194b983e417c6d7ed9
Leanne Eckelberg <leanne@code-inspector.com>
2021-01-24 16:12:23 -0700

    Adding new code

---
+++ pythoncode.py
@@ -0,0 +1,13 @@
+import logging
+class Person:
+   def __init__(self, firstname, lastname):
+       self.firstname = firstname
+       self.lastname = lastname
+def print_person(person):
+   if not person:
+       return None
+   else:
+       return "{0} {1}".format(person.firstname, person.lastname)
+p = Person("John", "Doe")
+print("first name of the object just created is {0}".format(p.firstname))
+print("using function: {0}".format(print_person(p)))
